d5a18f844947792bcdce0e81ef738d111957b339
KGriffey <griffey.kyle@gmail.com>
2021-12-27 12:38:21 -0700

    Added operate function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 // Base Functions //
 function add(a, b) {
-    return Number(a)+Number(b);
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
-    return a-b;
+    return a - b;
 }
 
 function multiply(a, b) {
-    return a*b;
+    return a * b;
 }
 
 function divide(a, b) {
-    return a/b;
+    return a / b;
 }
 
-/* Function Testing
+function operate(operator, a, b) {
+    if (operator == "add") {
+        return add(a, b);
+    } else if (operator == "subtract") {
+        return subtract(a, b);
+    } else if (operator == "multiply") {
+        return multiply(a, b);
+    } else {
+        return divide(a, b);
+    }
+}
+
+// Base Function Testing
+/*
 const a = prompt("a = ?");
 const b = prompt("b = ?");
 
@@ -24,3 +37,14 @@ console.log(subtract(a,b));
 console.log(multiply(a,b));
 console.log(divide(a,b));
 */
+
+// Operator Function Testing
+/*
+const operator = prompt("Operator: ");
+const a = prompt("a: ");
+const b = prompt("b: ");
+
+console.log(operate(operator, a, b));
+*/
+
+
