d3353bb5aa5dc4c10e4cddcea30812632881b0c2
KGriffey <griffey.kyle@gmail.com>
2021-12-27 12:26:23 -0700

    Added base functions and boilerplate

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Base Functions //
+function add(a, b) {
+    return Number(a)+Number(b);
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+/* Function Testing
+const a = prompt("a = ?");
+const b = prompt("b = ?");
+
+console.log(add(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
+*/
